9fe772d4a9a138b0d34ed3519d49d57a2d90cf38
Max <mjann@ucsc.edu>
2021-10-13 21:34:00 -0700

    greenyellow

--- nerves.js
+++ nerves.js
@@ -1,1 +1,15 @@
-alert("must be 21");
+// alert("must be 21");
+const maxcodes = ()=>{
+    const songy = document.getElementById("songx");
+    const mega = document.getElementById("sound");
+    const silent = document.getElementById("silence");
+    mega.addEventListener("click",()=>{
+        console.log("soundclick");
+        songy.play();
+    });
+    silent.addEventListener("click",()=>{
+        console.log("silentclick");
+        songy.pause();
+    });
+}
+maxcodes();
